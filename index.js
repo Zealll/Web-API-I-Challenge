d3cc5221717c992872b717c329cdb0185929a101
unknown <eriznis@gmail.com>
2019-03-25 15:35:47 -0400

    Project started

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');  
+// REQUIRE is the same thing as an import statement
+
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json())
+
+server.listen(4000, () => {
+    console.log('\n *** Server Running on http://localhost:4000 ***\n')
+})
