f2b501ba6032366b0a7c251e7db6d6d83b127c41
unknown <eriznis@gmail.com>
2019-03-25 16:16:42 -0400

    get call for all the objects built

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const server = express();
 
 server.use(express.json())
 
+server.get('/api/users', (req, res) => {
+    db
+    .find()
+    .then(individual => { res.status(200).json(individual) })
+    .catch(err => {
+        res
+        .status(500)
+        .json("The users information could not be retrieved.")
+    })
+})
+
+
+
+
+
+
 server.listen(4000, () => {
     console.log('\n *** Server Running on http://localhost:4000 ***\n')
 })
